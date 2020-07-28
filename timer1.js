54be343c503ea548dc4907b17b59fb97034d5371
Dominique Masena <dominique.masena@live.ca>
2020-07-28 19:46:29 +0000

    timer1.js

--- timer1.js
+++ timer1.js
@@ -1,9 +1,16 @@
 const seconds = process.argv.slice(2);
-const timer = function(times){
-  for (let time of times){
+const timer = function(times) {
+  
+   
+  for (let time   of times) {
+    if (time > 0 && !isNaN(time)) {
+    
     setTimeout(() => process.stdout.write('\x07'), time * 1000)
 
-  }
+    }
+}
 }
 
 timer(seconds);
+
+
